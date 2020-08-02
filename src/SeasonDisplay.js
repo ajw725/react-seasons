46706abce9748971cdda6bebfaf209b7b64b7747
Andrew Weinstein <andrew@altvia.com>
2020-08-02 17:16:38 -0600

    Start using state

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SeasonDisplay = () => {
+  return (
+    <div>I'm a season display</div>
+  );
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return (
-    <div>Hello</div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        this.setState({ lat: pos.coords.latitude });
+      },
+      (err) => console.error(err)
+    );
+  }
+
+  render() {
+    return (
+      <div>Latitude: {this.state.lat}</div>
+    );
+  }
 };
 
 ReactDOM.render(
