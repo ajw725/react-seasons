ccb4db26bb966f5bc249bb34a7cee572e1bd4ce7
Andrew Weinstein <andrew@altvia.com>
2020-08-15 17:05:32 -0600

    Refactor and style

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,28 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
-const SeasonDisplay = () => {
+const seasonConfig = {
+  summer: { text: "Let's hit the beach!", iconName: 'sun' },
+  winter: { text: "Brr, it's cold!", iconName: 'snowflake' }
+};
+
+const getSeason = (lat, month) => {
+  if(month >= 3 && month <= 8) {
+    return lat >= 0 ? 'summer' : 'winter';
+  }
+  return lat < 0 ? 'summer' : 'winter';
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+  const iconClass = `icon huge ${iconName}`;
   return (
-    <div>I'm a season display</div>
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left ${iconClass}`} />
+      <h1>{text}</h1>
+      <i className={`icon-right ${iconClass}`} />
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: '' };
 
-    this.state = { lat: null };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        this.setState({ lat: pos.coords.latitude });
-      },
-      (err) => console.error(err)
+      pos => this.setState({ lat: pos.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
+  }
+
+  renderContent() {
+    if(this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if(this.state.lat && !this.state.errorMessage) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner message="Please accept location request" />;
+
   }
 
   render() {
     return (
-      <div>Latitude: {this.state.lat}</div>
+      <div className="wrapper-class">
+        {this.renderContent()}
+      </div>
     );
   }
 };
